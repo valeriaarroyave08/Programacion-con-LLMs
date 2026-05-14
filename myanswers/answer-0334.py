f0197c3c52457d280a4b2a147c02cfe3008b232d
valeriaarroyave08 <valeria.arroyaveq@udea.edu.co>
2026-05-14 11:01:49 -0500

    Update answer-0334.py

--- myanswers/answer-0334.py
+++ myanswers/answer-0334.py
@@ -3,25 +3,25 @@ import numpy as np
 import random
 
 
-def generar_caso_de_uso_limpiar_datos():
+def generar_caso_de_uso_limpiar_datos(df=None):
     n_rows = random.randint(8, 15)
     n_cols = random.randint(3, 6)
 
     data = np.random.randn(n_rows, n_cols)
     cols = [f'col_{i}' for i in range(n_cols)]
 
-    df = pd.DataFrame(data, columns=cols)
-    df = pd.concat([df, df.iloc[:2]], ignore_index=True)
+    df_gen = pd.DataFrame(data, columns=cols)
+    df_gen = pd.concat([df_gen, df_gen.iloc[:2]], ignore_index=True)
 
-    for col in df.columns:
+    for col in df_gen.columns:
         if random.random() > 0.5:
-            df.loc[df.sample(frac=0.6).index, col] = np.nan
+            df_gen.loc[df_gen.sample(frac=0.6).index, col] = np.nan
 
     input_data = {
-        "df": df.copy()
+        "df": df_gen.copy()
     }
 
-    df_clean = df.drop_duplicates()
+    df_clean = df_gen.drop_duplicates()
     threshold = len(df_clean) * 0.5
     df_clean = df_clean.dropna(thresh=threshold, axis=1)
 
